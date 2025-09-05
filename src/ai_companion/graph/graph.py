e4a5bb9f8ac7be709732bc058fedec1f0f69def6
Rodiwa <rodiwaa@gmail.com>
2025-09-05 11:51:37 +0530

    tweaks

--- src/ai_companion/graph/graph.py
+++ src/ai_companion/graph/graph.py
@@ -33,29 +33,26 @@ def create_workflow_graph():
     graph_builder.add_node("audio_node", audio_node)
     graph_builder.add_node("summarize_conversation_node", summarize_conversation_node)
 
-    # Define the flow
+    ## Define the flow
     # First extract memories from user message
-    # graph_builder.add_edge(START, "memory_extraction_node") ## FIXME:
-    graph_builder.add_edge(START, "memory_extraction_node")
+    graph_builder.add_edge(START, "router_node")
 
     # Then determine response type
-    graph_builder.add_edge("memory_extraction_node", "router_node")
+    # graph_builder.add_edge("memory_extraction_node", "router_node")
 
     # Then inject both context and memories
-    # graph_builder.add_edge("router_node", "context_injection_node") # FIXME:
-    # graph_builder.add_edge("context_injection_node", "memory_injection_node") # FIXME:
+    # graph_builder.add_edge("router_node", "context_injection_node")
+    # graph_builder.add_edge("context_injection_node", "memory_injection_node")
     graph_builder.add_edge("router_node", "memory_injection_node")
 
 
     # Then proceed to appropriate response node
-    # graph_builder.add_conditional_edges("memory_injection_node", "select_workflow") ## FIXME:
     graph_builder.add_edge("memory_injection_node", "conversation_node")
 
     # Check for summarization after any response
-    # graph_builder.add_conditional_edges("conversation_node", should_summarize_conversation)
+    graph_builder.add_conditional_edges("conversation_node", should_summarize_conversation)
     # graph_builder.add_conditional_edges("image_node", should_summarize_conversation)
     # graph_builder.add_conditional_edges("audio_node", should_summarize_conversation)
-    graph_builder.add_conditional_edges("conversation_node", should_summarize_conversation)
     graph_builder.add_edge("summarize_conversation_node", END)
 
     return graph_builder
